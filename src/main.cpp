fe87c2ce330a54eea8da48299d164e173b9753cb
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-06-12 22:54:11 +0300

    crappy, but working start menu

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,28 @@ std::string file = "save.txt";
 Engine engine(80, 50);
 
 int main() {
-	if(TCODSystem::fileExists("save.txt")) {
+	// TODO this is so nasty I need to wash my hands
+	// show menu on startup
+
+	engine.gui->menu.clear();
+	engine.gui->menu.addItem(Menu::NEW_GAME, "New game");
+	if(TCODSystem::fileExists(file.c_str())) {
+		engine.gui->menu.addItem(Menu::CONTINUE, "Continue");
+	}
+	engine.gui->menu.addItem(Menu::EXIT, "Exit");
+	Menu::MenuItemCode menuItem = engine.gui->menu.pick();
+
+	if (menuItem == Menu::EXIT || menuItem == Menu::NONE) {
+		exit(0);
+	} else if (menuItem == Menu::NEW_GAME) {
+		engine.term();
+		engine.init();
+	} else {
+		engine.term();
 		std::ifstream ifs(file);
 		boost::archive::text_iarchive ia(ifs);
 		ia >> engine;
-	}
-	else {
-		engine.init();
+		engine.gameStatus = Engine::STARTUP;
 	}
 	while(!TCODConsole::isWindowClosed()) {
 		engine.update();
