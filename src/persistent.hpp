c4c57ccfb85098a4ee341f56d8980517dde0d52d
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-06-21 22:41:52 +0300

    fix actorsQueue serialization

--- src/persistent.hpp
+++ src/persistent.hpp
@@ -1,4 +1,5 @@
 #include <boost/serialization/unique_ptr.hpp>
+#include <boost/serialization/utility.hpp>
 
 // Non-intrusive serializations for TCOD classes
 template<class Archive>
@@ -10,15 +11,4 @@ inline void serialize (
 	ar & t.r;
 	ar & t.g;
 	ar & t.b;
-}
-
-// Non-intrusive serializations for std classes and structs
-template<class Archive>
-inline void serialize (
-	Archive & ar,
-	std::pair<float, Actor*> p,
-	const unsigned int file_version
-) {
-	ar & p.first;
-	ar & p.second;
 }
