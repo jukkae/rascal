a897c02481bd351ea85444bd16a1a357675dbf0c
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-07-03 20:17:33 +0300

    add tesla coil, because why not?

--- src/map.cpp
+++ src/map.cpp
@@ -54,26 +54,31 @@ void Map::addMonster(int x, int y) {
 
 void Map::addItem(int x, int y) {
 	int r = rng.getInt(0, 100);
-	if(r < 70) {
+	if(r < 60) {
 		Actor* stimpak = new Actor(x, y, '!', "stimpak", TCODColor::violet);
 		stimpak->blocks = false;
 		stimpak->pickable = std::unique_ptr<Pickable>(new Pickable(TargetSelector(TargetSelector::SelectorType::WEARER, 0), std::unique_ptr<Effect>(new HealthEffect(4))));
 		engine.actors.push_back(stimpak);
-	} else if(r < 80) {
+	} else if(r < 70) {
 		Actor* blasterBoltDevice = new Actor(x, y, '?', "blaster bolt device", TCODColor::lightYellow);
 		blasterBoltDevice->blocks = false;
 		blasterBoltDevice->pickable = std::unique_ptr<Pickable>(new Pickable(TargetSelector(TargetSelector::SelectorType::CLOSEST_MONSTER, 5), std::unique_ptr<Effect>(new HealthEffect(-20, "The %s is hit by a blast!\n The damage is %g hit points."))));
 		engine.actors.push_back(blasterBoltDevice);
-	} else if(r < 90) {
+	} else if(r < 80) {
 		Actor* fragGrenade = new Actor(x, y, '?', "fragmentation grenade", TCODColor::lightGreen);
 		fragGrenade->blocks = false;
 		fragGrenade->pickable = std::unique_ptr<Pickable>(new Pickable(TargetSelector(TargetSelector::SelectorType::SELECTED_RANGE, 3), std::unique_ptr<Effect>(new HealthEffect(-12, "The grenade explodes, hurting the %s for %g hit points!"))));
 		engine.actors.push_back(fragGrenade);
-	} else {
+	} else if(r < 90) {
 		Actor* confusor = new Actor(x, y, '?', "confusor", TCODColor::lightBlue);
 		confusor->blocks = false;
 		confusor->pickable = std::unique_ptr<Pickable>(new Pickable(TargetSelector(TargetSelector::SelectorType::SELECTED_MONSTER, 5), std::unique_ptr<Effect>(new AiChangeEffect(std::unique_ptr<TemporaryAi>(new ConfusedMonsterAi(10)), "The eyes of the %s look vacant!"))));
 		engine.actors.push_back(confusor);
+	} else {
+		Actor* teslaCoil = new Actor(x, y, '#', "tesla coil", TCODColor::lightBlue);
+		teslaCoil->blocks = false;
+		teslaCoil->pickable = std::unique_ptr<Pickable>(new Pickable(TargetSelector(TargetSelector::SelectorType::WEARER_RANGE, 5), std::unique_ptr<Effect>(new HealthEffect(-6, "The tesla coil sputters, emitting raw\n electricity, hurting %s for %g hp!"))));
+		engine.actors.push_back(teslaCoil);
 	}
 }
 
