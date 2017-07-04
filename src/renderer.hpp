75c3c62d236c5a03ed76aa1227188c98f528f6d2
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-07-04 21:22:44 +0300

    visibility

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -7,11 +7,12 @@ public:
 	Renderer() : screenWidth(80), screenHeight(50) {;}
 	Renderer(int screenWidth, int screenHeight): screenWidth(screenWidth), screenHeight(screenHeight) {;}
 	void render(const Map* const map, const std::vector<Actor*> actors) const;
-	void renderMap(const Map* const map) const; // should be const
-	void renderActor(const Actor* const actor) const;
 
 private:
 	int screenWidth;
 	int screenHeight;
+
+	void renderMap(const Map* const map) const;
+	void renderActor(const Actor* const actor) const;
 };
 #endif /* RENDERER_HPP */
