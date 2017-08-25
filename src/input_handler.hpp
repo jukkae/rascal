fa17d70554a8e4278d2ba3e2cf920694ab6a1e21
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-08-25 16:07:22 +0300

    add commentary about InputHandler's purpose

--- src/input_handler.hpp
+++ src/input_handler.hpp
@@ -1,6 +1,7 @@
 #ifndef INPUT_HANDLER_HPP
 #define INPUT_HANDLER_HPP
 class InputHandler {
-
+// TODO the point here being that "system" actions such as ESC is handled directly here,
+// and PlayerAi *requests* keypresses and other events pertinent to itself from InputHandler
 };
 #endif /* INPUT_HANDLER_HPP */
