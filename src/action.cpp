6608bfac25c1e5692fef8fc057329a13db1f5d77
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-08-25 14:58:14 +0300

    embed bumping into walls and attacking into moveAction

--- src/action.cpp
+++ src/action.cpp
@@ -1,17 +1,43 @@
 #include "action.hpp"
 #include "actor.hpp"
+#include "attacker.hpp"
+#include "gameplay_state.hpp"
 
-void MoveAction::execute() {
+bool MoveAction::execute() {
+	GameplayState* state = actor->s;
+	int targetX = actor->x;
+	int targetY = actor->y;
+
 	switch(direction) {
-		case Direction::N:  actor->y -= 1;                break;
-		case Direction::NE: actor->y -= 1; actor->x += 1; break;
-		case Direction::E:                 actor->x += 1; break;
-		case Direction::SE: actor->y += 1; actor->x += 1; break;
-		case Direction::S:  actor->y += 1;                break;
-		case Direction::SW: actor->y += 1; actor->x -= 1; break;
-		case Direction::W:                 actor->x -= 1; break;
-		case Direction::NW: actor->y -= 1; actor->x -= 1; break;
+		case Direction::N:  targetY -= 1;                break;
+		case Direction::NE: targetY -= 1; targetX += 1; break;
+		case Direction::E:                targetX += 1; break;
+		case Direction::SE: targetY += 1; targetX += 1; break;
+		case Direction::S:  targetY += 1;                break;
+		case Direction::SW: targetY += 1; targetX -= 1; break;
+		case Direction::W:                targetX -= 1; break;
+		case Direction::NW: targetY -= 1; targetX -= 1; break;
 		case Direction::NONE: break;
 		default: break;
 	}
+
+	if (state->isWall(targetX, targetY)) return false;
+
+	// look for living actors to attack
+	for (Actor* a : *actor->getActors()) {
+		if (a->destructible && !a->destructible->isDead() && a->x == targetX && a->y == targetY) {
+			actor->attacker->attack(actor, a);
+			return false;
+		}
+	}
+	// look for corpses or items
+	for (Actor* a : *actor->getActors()) {
+		bool corpseOrItem = (a->destructible && a->destructible->isDead()) || a->pickable;
+		if(corpseOrItem && a->x == targetX && a->y == targetY) {
+			state->message(TCODColor::lightGrey, "There's a %s here!", a->name.c_str());
+		}
+	}
+	actor->x = targetX;
+	actor->y = targetY;
+	return true;
 }
--- src/action.hpp
+++ src/action.hpp
@@ -16,7 +16,7 @@ enum class Direction { N, NE, E, SE, S, SW, W, NW, NONE };
 class Action {
 public:
 	Action(Actor* actor, float length = 100.0f) : actor(actor), length(length) {;}
-	virtual void execute() = 0; // TODO have action return bool whether it's possible or not, return ctrl to user if not
+	virtual bool execute() = 0; // TODO have action return bool whether it's possible or not, return ctrl to user if not
 	float getLength() { return length; }
 protected:
 	Actor* actor;
@@ -33,7 +33,7 @@ private:
 class EmptyAction : public Action {
 public:
 	EmptyAction(Actor* actor) : Action(actor, 200.0f) {;}
-	void execute() {std::cout<<"empty\n";}
+	bool execute() { std::cout<<"empty\n"; return true; }
 private:
 	friend class boost::serialization::access;
 	template<class Archive>
@@ -45,7 +45,7 @@ private:
 class MoveAction : public Action {
 public:
 	MoveAction(Actor* actor, Direction direction) : Action(actor, 100.0f), direction(direction) {;}
-	void execute();
+	bool execute();
 private:
 	Direction direction;
 
@@ -59,7 +59,7 @@ private:
 
 struct ActionResult {
 	bool succeeded;
-	boost::optional<Action> alternativeAction; // value would be preferable to pointer
+	boost::optional<Action> alternativeAction;
 };
 
 
