209602b996c50cbb83a6f14ca36fd8bf98ac9707
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-08-26 23:02:11 +0300

    handle ESC separately

--- src/input_handler.cpp
+++ src/input_handler.cpp
@@ -3,10 +3,28 @@
 
 #include "input_handler.hpp"
 
+#include "engine.hpp"
+#include "engine_command.hpp"
+#include "main_menu_state.hpp"
+
 void InputHandler::handleEvents(Engine* engine) {
 	TCOD_key_t key;
 	TCOD_mouse_t mouse;
-	TCOD_event_t ev = TCODSystem::checkForEvent(TCOD_EVENT_ANY, &key, &mouse);
+	TCOD_event_t ev = TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS | TCOD_EVENT_MOUSE, &key, &mouse);
+	if(ev == TCOD_EVENT_KEY_PRESS) {
+		switch(key.vk) {
+			case TCODK_ESCAPE: {
+				State* mainMenuState = new MainMenuState();
+				mainMenuState->init();
+				engine->pushState(mainMenuState);
+				return;
+			}
+			default: break;
+		}
+	}
+	if(ev == TCOD_EVENT_MOUSE) {
+
+	}
 	event = (RawInputEvent) { key, mouse, ev };
 }
 
