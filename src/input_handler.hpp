5077d807f0f315c7817c142fc449b53541987fe9
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-10-13 16:10:30 +0300

    pass mouse location from inputHandler

--- src/input_handler.hpp
+++ src/input_handler.hpp
@@ -2,6 +2,7 @@
 #define INPUT_HANDLER_HPP
 
 #include "boost/optional.hpp"
+#include "point.hpp"
 // The point here being that "system" actions such as ESC is handled directly here,
 // and PlayerAi *requests* keypresses and other events pertinent to itself from InputHandler
 struct RawInputEvent {
@@ -17,7 +18,9 @@ public:
 	InputHandler(Engine* engine) : engine(engine) {;}
 	void handleEvents();
 	boost::optional<RawInputEvent> getEvent(Actor* actor);
+	Point getMouseLocation();
 private:
+	Point lastMouseLocation = Point(0, 0);
 	Engine* engine;
 	boost::optional<RawInputEvent> event;
 };
