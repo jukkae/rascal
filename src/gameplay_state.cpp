37c5722d128f0b4670d2646411ec2610ca2d6803
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-10-13 10:05:14 +0300

    add debug prints

--- src/gameplay_state.cpp
+++ src/gameplay_state.cpp
@@ -1,5 +1,8 @@
 #include "gameplay_state.hpp"
 #include "engine.hpp"
+#include <chrono>
+#include <iostream>
+#include <boost/optional/optional_io.hpp>
 
 void GameplayState::init(Engine* engine) {
 	e = engine;
@@ -31,6 +34,7 @@ void GameplayState::init(Engine* engine) {
 	{
 		return lhs->energy > rhs->energy;
 	});
+
 
 	gui->message(TCODColor::green, "Welcome to year 20XXAD, you strange rascal!\nPrepare to fight or die!");
 }
@@ -72,6 +76,13 @@ void GameplayState::updateNextActor() {
     actors->erase(actors->begin());
     auto it = std::lower_bound(actors->begin(), actors->end(), activeActor, [](const auto& lhs, const auto& rhs) { return lhs->energy > rhs->energy; });
     actors->insert(it, activeActor);
+
+	// TODO debug
+	//std::cout << std::chrono::system_clock::now();
+	std::cout << "\n\n";
+	for(auto a : *actors) {
+		std::cout << a->name << " with energy " << a->energy << "\n";
+	}
     /*std::sort(actors->begin(), actors->end(), [](const auto& lhs, const auto& rhs)
     {
         return lhs->energy > rhs->energy;
