89bfdf383ca50a06e9aebda601b5f12f19f4856f
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-10-18 18:07:22 +0300

    formatting

--- src/attacker.cpp
+++ src/attacker.cpp
@@ -7,13 +7,13 @@
 Attacker::Attacker(float power) : power(power) {;}
 
 void Attacker::attack(Actor* owner, Actor* target) {
-if(target->destructible && !target->destructible->isDead()) {
-	if ( power - target->destructible->defense > 0 ) {
-		owner->s->message(TCODColor::red, "%s attacks %s for %g hit points.", owner->name.c_str(), target->name.c_str(), power - target->destructible->defense);
-	} else {
-		owner->s->message(TCODColor::lightGrey, "%s attacks %s but it has no effect!", owner->name.c_str(), target->name.c_str());            
-	}
-	target->destructible->takeDamage(target, power);
+	if(target->destructible && !target->destructible->isDead()) {
+		if ( power - target->destructible->defense > 0 ) {
+			owner->s->message(TCODColor::red, "%s attacks %s for %g hit points.", owner->name.c_str(), target->name.c_str(), power - target->destructible->defense);
+		} else {
+			owner->s->message(TCODColor::lightGrey, "%s attacks %s but it has no effect!", owner->name.c_str(), target->name.c_str());
+		}
+		target->destructible->takeDamage(target, power);
 	} else {
 		owner->s->message(TCODColor::lightGrey, "%s attacks %s in vain.", owner->name.c_str(), target->name.c_str());
 	}
