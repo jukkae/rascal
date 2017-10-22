f52f38ca46e64d896e620a4016024d495b2a944a
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-10-22 20:01:58 +0300

    add coordinate helper functions

--- src/rect.hpp
+++ src/rect.hpp
@@ -13,6 +13,14 @@ struct Rect {
 	// TODO checks that coordinates are in correct order (or just force them)
 	// TODO getters for raw coordinates + dimensions
 	Rect(int x0, int y0, int x1, int y1) : topLeft(Point(x0, y0)), bottomRight(Point(x1, y1)) {;}
+
+	// naming conventions & constness?
+	int x0() { return topLeft.x; }
+	int x1() { return bottomRight.x; }
+	int y0() { return topLeft.y; }
+	int y1() { return bottomRight.y; }
+	int width() { return x1()-x0() + 1; }
+	int height() { return y1()-y0() + 1; }
 };
 
 #endif /* RECT_HPP */
