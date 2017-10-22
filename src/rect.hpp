046494c9a1b3286ebb65f0565c1da6c8d0a38008
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-10-22 11:29:34 +0300

    add todos

--- src/rect.hpp
+++ src/rect.hpp
@@ -9,6 +9,9 @@ struct Rect {
 	Point bottomRight;
 
 	Rect(Point topLeft, Point bottomRight) : topLeft(topLeft), bottomRight(bottomRight) {;}
+	// TODO coordinate modes: tl + br, tl + dimensions, possibly center point + dimensions
+	// TODO checks that coordinates are in correct order (or just force them)
+	// TODO getters for raw coordinates + dimensions
 	Rect(int x0, int y0, int x1, int y1) : topLeft(Point(x0, y0)), bottomRight(Point(x1, y1)) {;}
 };
 
