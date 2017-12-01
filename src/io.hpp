2f1f8acf71e8e55ead9dc891ec807fc9c857be45
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-01 23:15:30 +0200

    add comments

--- src/io.hpp
+++ src/io.hpp
@@ -7,9 +7,10 @@ class Actor;
 class GameplayState;
 class Renderer;
 
+// TODO idea: instead of mousePosition, change to target that could be moved by, say, shift+arrows or tab
 namespace io {
 extern sf::RenderWindow window;
-extern Point mousePosition;
+extern Point mousePosition; // note: already in screen coordinates!
 bool waitForMouseClick(GameplayState* state);
 } // namespace io
 #endif /* IO_HPP */
