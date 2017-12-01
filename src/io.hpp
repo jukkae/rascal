6e1b80402205aaf8a95f4cdd4723dc7484e691f4
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-01 22:55:36 +0200

    add todo

--- src/io.hpp
+++ src/io.hpp
@@ -9,7 +9,7 @@ class Renderer;
 
 namespace io {
 extern sf::RenderWindow window;
-extern Point mousePosition;
+extern Point mousePosition; // TODO this is not always updated correctly - seems that second click works
 bool pickTile(GameplayState* state, Renderer* renderer, Actor* actor, int* x, int* y, float maxRange = 0.0f);
 bool waitForMouseClick(GameplayState* state);
 } // namespace io
