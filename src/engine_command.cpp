c0c12d5b90f53ed7fee1acf6fb76de86ba43a636
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-30 22:35:02 +0200

    start work on changestatecommand

--- src/engine_command.cpp
+++ src/engine_command.cpp
@@ -1,5 +1,6 @@
 #include "engine_command.hpp"
 #include "engine.hpp"
+#include "state.hpp"
 
 void NewGameCommand::execute() {
 	engine->newGame();
@@ -10,4 +11,12 @@ void ContinueCommand::execute() {
 }
 void ExitCommand::execute() {
 	engine->exit();
+}
+
+ChangeStateCommand::ChangeStateCommand(Engine* engine, std::unique_ptr<State> state) :
+engine(engine), state(std::move(state)) {;}
+
+void ChangeStateCommand::execute() {
+	engine->popState();
+	engine->pushState(std::move(state));
 }
