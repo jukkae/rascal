2fda0751c93244073f8fef736d2526693784488e
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-31 13:38:07 +0200

    format

--- src/io.hpp
+++ src/io.hpp
@@ -7,10 +7,12 @@ class Actor;
 class GameplayState;
 class Renderer;
 
-// TODO idea: instead of mousePosition, change to target that could be moved by, say, shift+arrows or tab
 namespace io {
+
 extern sf::RenderWindow window;
+// TODO idea: instead of mousePosition, change to target that could be moved by, say, shift+arrows or tab
 extern Point mousePosition; // note: already in WORLD coordinates!
+
 bool waitForMouseClick(GameplayState* state);
 bool fileExists(std::string name);
 } // namespace io
