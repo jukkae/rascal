62a1c34be94dc3c239e6275e784b4b1f967cff6a
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2017-12-31 15:48:48 +0200

    better conditional font loading

--- src/font.cpp
+++ src/font.cpp
@@ -9,11 +9,8 @@ sf::Font font::mainFont;
 sf::Font font::squareFont;
 
 extern void font::load() {
-#ifdef BUNDLE_BUILD
-	//std::cout << "BUNDLE BUILD DETECTED\n";
-#endif
 #ifdef __APPLE__
-	// FIXME this ifdef only when building appbundle!
+# ifdef BUNDLE_BUILD
 	CFBundleRef mainBundle = CFBundleGetMainBundle();
     CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
     char path[PATH_MAX];
@@ -38,6 +35,14 @@ extern void font::load() {
 			std::cout << "retry failed\n";
 		}
 	}
+# else
+	if(!font::mainFont.loadFromFile("assets/FSEX300.ttf")) {
+		std::cout << "error loading main font\n";
+	}
+	if(!font::squareFont.loadFromFile("assets/square.ttf")) {
+		std::cout << "error loading square font\n";
+	}
+# endif /* BUNDLE_BUILD */
 #elif __linux__
 	if(!font::mainFont.loadFromFile("assets/FSEX300.ttf")) {
 		std::cout << "error loading main font\n";
