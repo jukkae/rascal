6cf41ba4e4ac204024013d3e73374981df614fc4
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-01-05 11:42:17 +0200

    add comment

--- src/ignore.hpp
+++ src/ignore.hpp
@@ -2,6 +2,7 @@
 #define IGNORE_HPP
 
 //Use to silence compiler warnings and be explicit about not using it
+//My clang doesn't yet support [[maybe_unused]] attribute, should use that when available
 template <typename T>
 inline void ignore(T&&) { }
 
