10fc848151f6f6e0f27ecfcb0936e4b3b13bda8f
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-01-09 08:33:18 +0200

    virtual dtors for ABCs

--- src/effect.hpp
+++ src/effect.hpp
@@ -8,6 +8,7 @@ class StatusEffect;
 
 class Effect {
 public:
+	virtual ~Effect() {}
 	virtual bool applyTo(Actor* actor) = 0;
 private:
 	friend class boost::serialization::access;
@@ -83,6 +84,7 @@ private:
 class EffectGenerator {
 public:
 	virtual std::unique_ptr<Effect> generateEffect() = 0;
+	virtual ~EffectGenerator() {}
 private:
 	friend class boost::serialization::access;
 	template<class Archive>
