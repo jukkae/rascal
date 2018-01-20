47997e3a1cc3fcac147916ac17dcec47bd0c2e03
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-01-20 04:01:58 +0200

    increase score from kills

--- src/player.cpp
+++ src/player.cpp
@@ -5,6 +5,11 @@
 
 void Player::notify(Event& event) {
 	if(auto e = dynamic_cast<PlayerStatChangeEvent*>(&event)) {
+		ignore(e);
 		score += 10;
+	}
+	if(auto e = dynamic_cast<DeathEvent*>(&event)) {
+		ignore(e);
+		score += 1;
 	}
 }
