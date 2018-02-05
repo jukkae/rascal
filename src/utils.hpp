fee2a63bf6ef9bffb4342140e8ce07d989441013
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-02-05 16:13:22 +0200

    add namespace end comment

--- src/utils.hpp
+++ src/utils.hpp
@@ -6,5 +6,5 @@ template<class Container, class F>
 auto erase_where(Container& c, F&& f) {
 	return c.erase(std::remove_if(c.begin(), c.end(), std::forward<F>(f)), c.end());
 }
-};
+}; // namespace utils
 #endif /* UTILS_HPP */
