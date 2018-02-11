94300c0a37e53be643b544bdb3b2e83ad0884e0f
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-02-11 14:11:19 +0200

    bump version to development status

--- src/version.hpp
+++ src/version.hpp
@@ -2,7 +2,7 @@
 #define VERSION_HPP
 
 namespace version {
-static std::string const VERSION = "0.0.2";
+static std::string const VERSION = "0.0.2-develop";
 } // namespace version
 
 #endif /* VERSION_HPP */
