cb449e5757c2b1c1969362243ed3ed6412dba200
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-02-12 21:54:27 +0200

    add console type and cell structs

--- src/console.hpp
+++ src/console.hpp
@@ -3,8 +3,17 @@
 
 #include "colors.hpp"
 #include "point.hpp"
+enum class ConsoleType { SQUARE, NARROW };
+
+struct Cell {
+	sf::Color bg;
+	sf::Color fg;
+	char glyph; // is char good?
+};
+
 class Console {
 public:
+	Console() : consoleType(ConsoleType::SQUARE) {;}
 	void draw();
 	void setBackground(Point position, sf::Color color);
 	void drawGlyph(Point position, std::string glyph, sf::Color color);
@@ -12,6 +21,8 @@ public:
 private:
 	//internal 2d buffer of bg/fg colors & glyphs to be drawn
 	//-> custom 2d array type?
+	std::vector<Cell> cells;
+	ConsoleType consoleType;
 };
 
 #endif /* CONSOLE_HPP */
