224f3da679ed6ba58953b3f479a66f8e191d9d02
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-02-12 16:10:51 +0200

    start working on virtual console

---
+++ src/console.cpp
@@ -0,0 +1,5 @@
+#include "console.hpp"
+
+void Console::draw() {
+
+}
---
+++ src/console.hpp
@@ -0,0 +1,10 @@
+#ifndef CONSOLE_HPP
+#define CONSOLE_HPP
+
+class Console {
+public:
+	void draw();
+private:
+};
+
+#endif /* CONSOLE_HPP */
