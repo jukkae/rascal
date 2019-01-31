2894fc9223bd78c53c26333a3e855a4bc1dcf5d1
Jukka Eerikäinen <jukkae@iki.fi>
2019-01-31 09:32:00 +0200

    pass actors vec to fov calculations again

--- src/ai.cpp
+++ src/ai.cpp
@@ -41,10 +41,10 @@ const int LEVEL_UP_FACTOR = 150;
 void Ai::updateFov(Actor* actor) {
 	float fovRadius = constants::DEFAULT_FOV_RADIUS * (actor->body ? actor->body->perception / 10.0 : 1.0);
 	if(isPlayer()) {
-		fov = fov::computeFov(&(actor->world->map), actor->x, actor->y, fovRadius);
+		fov = fov::computeFov(&(actor->world->map), actor->x, actor->y, fovRadius, FovType::CIRCLE, actor->world->getActorsAsPtrs());
 	}
 	else {
-		fov = fov::computeEnemyFov(&(actor->world->map), actor->x, actor->y, currentDirection, fovRadius);
+		fov = fov::computeEnemyFov(&(actor->world->map), actor->x, actor->y, currentDirection, fovRadius, FovType::CIRCLE, actor->world->getActorsAsPtrs());
 	}
 }
 
