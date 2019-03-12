5e9a86e58153cfa9d9e813231caef14b7b9004e6
Jukka Eerikäinen <jukkae@iki.fi>
2019-03-12 10:48:00 +0200

    get meson bundle build working

--- tools/make_bundle.sh
+++ tools/make_bundle.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
+# Generate iconset
+rm -rf macosx/Rascal.iconset
+mkdir macosx/Rascal.iconset
+sips -z 16 16     macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_16x16.png
+sips -z 32 32     macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_16x16@2x.png
+sips -z 32 32     macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_32x32.png
+sips -z 64 64     macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_32x32@2x.png
+sips -z 128 128   macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_128x128.png
+sips -z 256 256   macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_128x128@2x.png
+sips -z 256 256   macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_256x256.png
+sips -z 512 512   macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_256x256@2x.png
+sips -z 512 512   macosx/RascalIcon.png --out macosx/Rascal.iconset/icon_512x512.png
+cp macosx/RascalIcon.png macosx/Rascal.iconset/icon_512x512@2x.png
+iconutil -c icns -o macosx/Rascal.icns macosx/Rascal.iconset
+rm -r macosx/Rascal.iconset
+
+# Delete previous version
 rm -rf Rascal.app
 
+# Build directory structure
 mkdir Rascal.app
 mkdir Rascal.app/Contents
 mkdir Rascal.app/Contents/MacOS
@@ -11,17 +29,22 @@ mkdir Rascal.app/Contents/Resources/Frameworks
 mkdir Rascal.app/Contents/Frameworks
 mkdir Rascal.app/Contents/Resources/Assets
 
+# Copy bundle-required metadata
 cp macosx/Info.plist Rascal.app/Contents
 cp macosx/PkgInfo Rascal.app/Contents
 cp macosx/Rascal.icns Rascal.app/Contents/Resources
+
+# Copy executable
 cp build/rascal_for_bundle Rascal.app/Contents/MacOS/Rascal
 
+
+# Bundle assets and dependencies
 cp -r ./assets/ Rascal.app/Contents/Resources/Assets/
 cp -r ./frameworks/ Rascal.app/Contents/Resources/Frameworks/
 cp -r ./frameworks/ Rascal.app/Contents/Frameworks/
 
-install_name_tool -change @executable_path/../Frameworks/SFML.framework/Versions/2.4.2/SFML @executable_path/../Resources/Frameworks/SFML.framework/Versions/2.4.2/SFML ./Rascal.app/Contents/MacOS/Rascal
-install_name_tool -change @executable_path/../Frameworks/sfml-window.framework/Versions/2.4.2/sfml-window @executable_path/../Resources/Frameworks/sfml-window.framework/Versions/2.4.2/sfml-window ./Rascal.app/Contents/MacOS/Rascal
-install_name_tool -change @executable_path/../Frameworks/sfml-graphics.framework/Versions/2.4.2/sfml-graphics @executable_path/../Resources/Frameworks/sfml-graphics.framework/Versions/2.4.2/sfml-graphics ./Rascal.app/Contents/MacOS/Rascal
-install_name_tool -change @executable_path/../Frameworks/sfml-system.framework/Versions/2.4.2/sfml-system @executable_path/../Resources/Frameworks/sfml-system.framework/Versions/2.4.2/sfml-system ./Rascal.app/Contents/MacOS/Rascal
-install_name_tool -change @executable_path/../Frameworks/sfml-audio.framework/Versions/2.4.2/sfml-audio @executable_path/../Resources/Frameworks/sfml-audio.framework/Versions/2.4.2/sfml-audio ./Rascal.app/Contents/MacOS/Rascal
+# Fix rpath
+../mac-third-party-libs-tool/third_party_libs_tool ./Rascal.app
+
+# Zip bundle
+zip -qr rascal.zip Rascal.app/ -x "*.DS_Store"
